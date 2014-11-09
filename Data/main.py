8a34c2efd8164f9cc4fcc1214697329fae075548
bigbeno37 <bigbeno37@gmail.com>
2014-11-09 09:41:29 +0000

    Finished game. Time to submit

--- Data/main.py
+++ Data/main.py
@@ -3,24 +3,25 @@ from sys import exit
 
 class Main(object):
     debugMode = False
+    name = ""
     
     def setDebugMode(self, debug):
         self.debugMode = debug
     
     def intro(self):      
-        print("Welcome to WaterWorks v0.1!")
+        print("Welcome to WaterWorks v1.7b!")
         try:
-            name = raw_input("What is your name? ")
+            self.name = raw_input("What is your name? ")
         except NameError:
-            name = input("What is your name? ")
-        print("Hello, " + name + ". Time to begin!")
+            self.name = input("What is your name? ")
+        print("Hello, " + self.name + ". Time to begin!")
         sleep(2) if self.debugMode == False else sleep(0)
     
-    def displayText(self, text):
+    def displayText(self, text, time=2):
         if self.debugMode == False:
             for element in text:
                 print(element)
-                sleep(2)
+                sleep(time)
                 
     def answers(self, question, answer):
         try:
@@ -37,7 +38,17 @@ class Main(object):
         print("Command not recognised!")
         self.answers(x, y)
         
-    def endGame(self, points):
-        print("Unfortunately, you did not make it to the end of the game")
-        print("You reached a score of " + str(points) + "!")
+    def endGame(self, points, bad_end=False, good_end=False):
+        if bad_end == False and good_end == False:
+            print("Unfortunately, you did not make it to the end of the game")
+            print("You reached a score of " + str(points) + "!")
+        elif bad_end == True:
+            print("BAD END")
+            self.credits()
+        elif good_end == True:
+            print("GOOD END")
+            self.credits()
+
+    def credits(self):
+        self.displayText(["WaterWorks", "Created and debugged by Ben O'Sullivan", "Inspiration from the works of Oliver MacPherson", "Thanks for playing " + self.name + "!"], 4)
         exit()
--- play.py
+++ play.py
@@ -1,6 +1,7 @@
 from Data.main import Main
 from sys import exit
 from os import stat
+from time import sleep
 
 m = Main()
 
@@ -12,51 +13,95 @@ m.setDebugMode(False)
 
 m.intro()
 
-m.displayText(textArray[1:5])
-question1 = m.answers(questionArray[0], answerArray[0:2])
-if question1 == 'reachforthepipe':
-    m.displayText(textArray[7:10])
+def game():
+    m.displayText(textArray[1:5])
+    question1 = m.answers(questionArray[0], answerArray[0:2])
+    if question1 == 'reachforthepipe':
+        m.displayText(textArray[7:10])
 
-m.displayText(textArray[11:14])
-if m.answers(questionArray[2], answerArray[3:5]) == 'godowntherightpath':
-    m.displayText(textArray[15:20])
-    m.endGame(50)
+    m.displayText(textArray[11:14])
+    if m.answers(questionArray[2], answerArray[3:5]) == 'godowntherightpath':
+        m.displayText(textArray[15:20])
+        m.endGame(50)
+        replay()
 
-m.displayText(textArray[21:28])
-answer = m.answers(questionArray[4], answerArray[6:9])
-if answer == 'opentherightdoor':
-    m.displayText(textArray[29:33])
-    m.endGame(100)
-elif answer == 'iknowwhatitsays':
-    try:
-        if raw_input("What does it mean? ").replace(' ', '').lower() != 'youmustgointotheleftdoor':
-            print("All of a sudden, you hear a voice in your head saying INCORRECT")
-            print("You feel faint, dizzy...")
-            print("You pass out")
-            m.endGame(150)
-        else:
-            print("All of a sudden, you hear a voice in your head saying CORRECT!")
-    except NameError:
-        if input("What does it mean? ").replace(' ', '').lower() != 'youmustgointotheleftdoor':
-            print("All of a sudden, you hear a voice in your head saying INCORRECT")
-            print("You feel faint, dizzy...")
-            print("You pass out")
-            m.endGame(150)
-        else:
-            print("All of a sudden, you hear a voice in your head saying CORRECT!")
+    m.displayText(textArray[21:28])
+    answer = m.answers(questionArray[4], answerArray[6:9])
+    if answer == 'opentherightdoor':
+        m.displayText(textArray[29:33])
+        m.endGame(100)
+        replay()
+    elif answer == 'iknowwhatitsays':
+        try:
+            if raw_input("What does it mean? ").replace(' ', '').lower() != 'youmustgointotheleftdoor':
+                print("All of a sudden, you hear a voice in your head saying INCORRECT")
+                sleep(2)
+                print("You feel faint, dizzy...")
+                sleep(2)
+                print("You pass out")
+                sleep(2)
+                m.endGame(150)
+                replay()
+            else:
+                print("All of a sudden, you hear a voice in your head saying CORRECT!")
+        except NameError:
+            if input("What does it mean? ").replace(' ', '').lower() != 'youmustgointotheleftdoor':
+                print("All of a sudden, you hear a voice in your head saying INCORRECT")
+                sleep(2)
+                print("You feel faint, dizzy...")
+                sleep(2)
+                print("You pass out")
+                sleep(2)
+                m.endGame(150)
+                replay()
+            else:
+                print("All of a sudden, you hear a voice in your head saying CORRECT!")
         
-m.displayText(textArray[34:42])
-answer = m.answers(questionArray[6], answersArray[10:12])
-if answer == 'gototheabandonedhouse':
-    m.displayText(textArray[43:48])
-    m.endGame(200)
+    m.displayText(textArray[34:42])
+    answer = m.answers(questionArray[6], answerArray[10:12])
+    if answer == 'gototheabandonedhouse':
+        m.displayText(textArray[43:48])
+        m.endGame(200)
+        replay()
 
-m.displayText(textArray[49:57])
-if m.answers(questionArray[8], answersArray[13:15]) == 'pullthelever':
-    print("You pull the lever, but nothing happens")
-else:
-    print("You push the button, but nothing happens")
+    m.displayText(textArray[49:57])
+    if m.answers(questionArray[8], answerArray[13:15]) == 'pullthelever':
+        print("You pull the lever, but nothing happens")
+    else:
+        print("You push the button, but nothing happens")
     
-m.displayText(textArray[58:64])
-if m.answers(questionArray[10], answesArray[13:15]) == 'gointothebuilding':
+    m.displayText(textArray[58:64])
+    if m.answers(questionArray[10], answerArray[19:21]) == 'gointothebuilding':
+        m.displayText(textArray[65:72])
+        m.endGame(250)
+        replay()
+
+    m.displayText(textArray[73:79])
+    if m.answers(questionArray[12], answerArray[16:18]) == 'gototheplains':
+        m.displayText(textArray[80:91])
+        m.endGame(300)
+        replay()
     
+    m.displayText(textArray[92:104], 3)
+    if m.answers(questionArray[14], answerArray[22:24]) == 'preparetoliveinthewild':
+        m.displayText(textArray[105:115])
+        m.endGame(350, True)
+        replay()
+        
+    m.displayText(textArray[116:126])
+    m.endGame(400, False, True)
+    replay()
+        
+def replay():
+    try:
+        if raw_input("Do you want to play again? (Yes / No) ").lower() == 'yes':
+            game()
+        else:
+            exit()
+    except NameError:
+        if input("Do you want to play again? (Yes / No) ").lower() == 'yes':
+            game()
+        else:
+            exit()
+
+game()
